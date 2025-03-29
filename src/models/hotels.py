60e90f2d7f6967b39a2687e637bc2b509b9c11c2
TatyanaStartseva <TatyanaStartseva2020@yandex.ru>
2025-03-29 14:19:50 +0300

    добавлена орм для комнат

--- src/models/hotels.py
+++ src/models/hotels.py
@@ -3,7 +3,7 @@ from src.database import Base
 from sqlalchemy import String
 
 class HotelsOrm(Base):
-    __tablename__ = "hostels"
+    __tablename__ = "hotels"
     id:Mapped[int]=  mapped_column(primary_key=True)
     title:Mapped[str] =mapped_column(String(100))
     location: Mapped[str]=mapped_column
